da8dd78be956dffe0270fb47298cf187b4674693
Vamsi_Anupoju <150006429+vamsi5355@users.noreply.github.com>
2025-10-01 07:46:14 +0530

    Time: 8 ms (0.14%) | Memory: 41.2 MB (6.30%) - LeetSync

--- 1642-water-bottles/water-bottles.java
+++ 1642-water-bottles/water-bottles.java
@@ -5,6 +5,7 @@ class Solution {
             int rem=0;
             if(numBottles>=numExchange)
             rem=numBottles%numExchange;
+            System.out.println(numBottles+" "+rem);
             numBottles=(numBottles/numExchange);
             ans+=numBottles;
             numBottles+=rem;

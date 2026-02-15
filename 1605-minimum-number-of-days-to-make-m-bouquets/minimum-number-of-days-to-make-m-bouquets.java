d87c676b0f15144c722ab510cef5b6a745309cdc
Anupoju Madhu Vamsi <23a91a0504@aec.edu.in>
2026-02-15 18:53:05 +0530

    Time: 15 ms (96.75%) | Memory: 82.5 MB (89.96%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,32 +1,44 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
-        if(bloomDay.length < (long)m*k)return -1;
-        int l=Arrays.stream(bloomDay).min().getAsInt(),r=Arrays.stream(bloomDay).max().getAsInt(),ans=l;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(check(bloomDay,k,mid)>=m){
-                ans=mid;
-                r=mid-1;
-            }
-            else{
-                l=mid+1;
+        if (bloomDay.length < (long)m * k) return -1;
+
+        int l = 1, r = 0;
+        for (int day : bloomDay) {
+            r = Math.max(r, day);
+        }
+
+        int ans = -1;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            if (canMake(bloomDay, m, k, mid)) {
+                ans = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
             }
         }
+
         return ans;
     }
-    public int check(int[] b,int k,int cur){
-        int cl=0,md=0;
-            for(int i=0;i<b.length;i++){
-                    if(b[i]<=cur){
-                        cl++;
-                    }else{
-                        cl=0;
-                    }
-                    if(cl==k){
-                        md++;
-                        cl=0;
-                    }
+
+    private boolean canMake(int[] b, int m, int k, int cur) {
+        int count = 0, bouquets = 0;
+
+        for (int i = 0; i < b.length; i++) {
+            if (b[i] <= cur) {
+                count++;
+                if (count == k) {
+                    bouquets++;
+                    if (bouquets >= m) return true;  
+                    count = 0;
+                }
+            } else {
+                count = 0;
             }
-            return md;
+        }
+
+        return false;
     }
 }

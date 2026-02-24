688fb6d9d75e917c7952a37d83897e51f54d7e90
Anupoju Madhu Vamsi <23a91a0504@aec.edu.in>
2026-02-24 19:21:51 +0530

    Time: 1 ms (99.73%) | Memory: 44.2 MB (71.89%) - LeetSync

---
+++ 257-binary-tree-paths/binary-tree-paths.java
@@ -0,0 +1,26 @@
+class Solution {
+    List<String> res = new ArrayList<>();
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        backtrack(root, new StringBuilder());
+        return res;
+    }
+
+    public void backtrack(TreeNode node, StringBuilder sb) {
+        if (node == null) return;
+
+        int len = sb.length();  
+
+        sb.append(node.val);
+
+        if (node.left == null && node.right == null) {
+            res.add(sb.toString());
+        } else {
+            sb.append("->");
+            backtrack(node.left, sb);
+            backtrack(node.right, sb);
+        }
+
+        sb.setLength(len);  
+    }
+}
